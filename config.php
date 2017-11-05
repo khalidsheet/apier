62b5883a6adb732bf80329add552598beb876713
Khalid Mohammad <prog.khalid.mohammad@gmail.com>
2017-11-05 13:53:47 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,41 @@
+<?php
+
+// database configuration
+$db = [
+	'mysql' => [
+		'HOST' => '127.0.0.1',
+		'NAME' => 'apier_db',
+		'USER' => 'root',
+		'PASS' => ''
+	]
+];
+
+
+$config = array(
+    'driver'    => 'mysql', // Db driver
+    'host'      => $db['mysql']['HOST'],
+    'database'  => $db['mysql']['NAME'],
+    'username'  => $db['mysql']['USER'],
+    'password'  => $db['mysql']['PASS'],
+    'charset'   => 'utf8', // Optional
+    'collation' => 'utf8_unicode_ci', // Optional
+    'prefix'    => '', // Table prefix, optional
+);
+
+
+$mailConfig = [
+    'host' => 'smtp.gmail.com',
+    'username' => 'test@domain.com',
+    'password' => 'secret',
+    'SMTPSecure' => 'tls',
+    'port' => 587,
+
+    // Set From E-mail
+    'your_email' => 'fromEmail@gmail.com',
+
+    // Leave it empty if you want to show email address
+    'alias_name' => 'Test'
+];
+
+
+
